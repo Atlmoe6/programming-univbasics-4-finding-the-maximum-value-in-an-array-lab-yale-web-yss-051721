fd7a92a9a6b216dcf1f7d392abaf4edcffd4a585
abstract-inheritance-c040 <doe.mahamud1@gmail.com>
2021-05-28 08:26:08 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,10 @@
 def find_max_value(array)
   count = 0 
-  maximum_value = 5
+  maximum_value = 0
   while count < array.size do 
-    
-  # Add your solution here
+    if maximum_value < array[count]
+      maximum_value = array[count]
+    end 
+    count += 1
+  maximum_value
 end
